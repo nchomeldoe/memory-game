5f6855ce14481ce8e9519a32705a84f7b967240d
nchomeldoe <nchomeldoe@gmail.com>
2021-06-30 20:58:18 +0100

    created a function to shuffle the tiles

--- script/script.js
+++ script/script.js
@@ -1,23 +1,48 @@
-const tilesFront = document.querySelectorAll(".tile-front");
-const tilesBack = document.querySelectorAll(".tile-back");
-const images = document.querySelectorAll(".img");
+//Query selectors
+
+const tiles = document.querySelectorAll(".tile");
+console.dir(tiles);
+
+const svg1 = document.querySelector("#svg-1");
+console.dir(svg1);
+
+//start game when page loads
+
+document.body.onload = startGame();
 
 
-tilesFront.forEach((tileFront) => {
-    tileFront.addEventListener("click", () => {
-        tileFront.parentNode.style.transform = "rotateY(180deg)";
-    })
-})
+//start game function
 
+function startGame() {
 
-tilesBack.forEach((tileBack) => {
-    tileBack.addEventListener("click", () => {
-        tileBack.style.transform = "rotateY(180deg)";
-    })
-})
+//shuffle the tiles
 
-// images.forEach((image) => {
-//     image.addEventListener("click", () => {
-//         image.parentNode.style.transform = "rotateY(180deg)";
-//     })
-// })
+const imageCategories = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F"];
+
+for(let i = tiles.length-1; i>=0; i--) {
+    let randomNumber = Math.floor(Math.random() * (i+1));
+    tiles[i].className += ` ${imageCategories[randomNumber]}`;
+    imageCategories.splice(randomNumber, 1);
+    switch(tiles[i].className) {
+        case "tile A":
+            tiles[i].childNodes[1].src = "images/1.svg";
+            break;
+        case "tile B":
+            tiles[i].childNodes[1].src = "images/2.svg";
+            break;
+        case "tile C":
+            tiles[i].childNodes[1].src = "images/3.svg";
+            break;
+        case "tile D":
+            tiles[i].childNodes[1].src = "images/4.svg";
+            break;
+        case "tile E":
+            tiles[i].childNodes[1].src = "images/5.svg";
+            break;
+        case "tile F":
+            tiles[i].childNodes[1].src = "images/6.svg";
+            break;
+    }
+};
+
+}
