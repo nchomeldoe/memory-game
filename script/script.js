2344713fca8e11021f1e9c3c1bd323d62a164fd2
nchomeldoe <nchomeldoe@gmail.com>
2021-07-02 19:57:32 +0100

    added functions for when a pair of tiles matches and when it doesn't

--- script/script.js
+++ script/script.js
@@ -5,9 +5,9 @@ const flippedTiles = document.querySelectorAll(".open");
 
 //Set counters
 
-const openTiles = [];
-const goesTaken = 0;
-const matchedPairs = 0;
+let openTiles = [];
+let goesTaken = 0;
+let matchedPairs = 0;
 
 //start game when page loads
 
@@ -48,52 +48,69 @@ function startGame() {
         }
     };
 
-//     flip up to two tiles and add to classlist
+
 
     tiles.forEach((tile) => {
         tile.addEventListener("click", () => {
+
+            //     flip up to two tiles and add to classlist
             if(openTiles.length < 2){
                 openTiles.push(tile.className);
                 tile.classList.add("open");
             }
+            console.log(openTiles.length)
+
+            //     when two tiles are open, increment goesTaken and check for match
+            if(openTiles.length === 2){
+                goesTaken += 1;
+                console.log(goesTaken);
+                if(openTiles[0] !== openTiles[1]){
+                    tilesUnmatched();
+                }
+                else{
+                    tilesMatched();
+                }
+            }
+        
+            //  if open tiles len is 2 then:
+                // increment goesTaken
+                // check for match then
+                //  either do the matched func (including incrementing matchedPairs and adding matched class to open tiles) 
+                // or the unmatched func
+                // clear openTiles
+                //  if goes taken > [value of difficulty] then throw you lose panel
+                // if matched pairs == 6 then throw you win panel
             console.log(openTiles);
         })
     });
 
-
+// event listener on reset button
 }
 
-// flip tiles on click and increment openTiles counter
+// sleep function
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
-// function flipTiles() {
-//     tiles.forEach((tile) => {
-//         tile.addEventListener("click", () => {
-//             openTiles.push(tile.className);
-//             tile.classList.add("open");
-//         })
-//     });
-// };
+  //if tiles are not matched, flip them back and clear openTiles
+ async function tilesUnmatched() {
+    await sleep(1500);
+     tiles.forEach((tile) => {
+         if(tile.classList.contains("open")) {
+            tile.classList.remove("open");
+         }
+         })
+        openTiles = []
+     }
 
-//stop flipping tiles when openTiles array reaches a length of 2
-
-// function playGame() {
-//     if(openTiles.length < 2){
-//         flipTiles()
-//     }
-//     else{
-//         goesTaken += 1;
-//         if(openTiles[0] === openTiles[1]){
-//             matchedPairs += 1;
-//             flippedTiles.forEach((tile) => {
-//                 tile.classList.remove("open");
-//                 tile.backgroundColor = "green";
-//             })
-//         }
-//         else {
-//             flippedTiles.forEach((tile) => {
-//                 tile.classList.remove("open");
-//                 tile.backgroundColor = "red";
-//             })
-//         }
-//     }
-// }
+  //if tiles are matched, keep them open and change to green and increment matchedPairs
+  async function tilesMatched() {
+    await sleep(1500);
+     tiles.forEach((tile) => {
+         if(tile.classList.contains("open")) {
+            tile.classList.remove("open");
+            tile.classList.add("matched");
+         }
+         })
+        openTiles = []
+     }
