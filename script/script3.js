106f42cae639dfea31fc5f6463dc19bb2e7fa22c
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-07-15 17:05:14 +0100

    applied BEM naming convention and changed px vcalues to rem

--- script/script3.js
+++ script/script3.js
@@ -1,7 +1,7 @@
 //Query selectors
 
-const tiles = document.querySelectorAll(".tile");
-const newGame = document.querySelector(".refresh-button");
+const tiles = document.querySelectorAll(".board__tile");
+const newGame = document.querySelector(".reset__button");
 const body = document.querySelector("body");
 const notification = document.querySelector(".notification");
 const playAgain = document.querySelector(".play-again");
@@ -17,150 +17,152 @@ let matchedPairs = 0;
 // sleep function
 
 const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
- // set popup message depending how many goes have been taken to complete game
+// set popup message depending how many goes have been taken to complete game
 
 const renderFinalScreen = (goesTaken) => {
-    switch (true) {
-        case goesTaken < 10:
-            return `Congratulations! You finished the game in ${goesTaken} goes!`;
-        case goesTaken < 14:
-            return `Good effort! You finished the game in ${goesTaken} goes!`;
-        case goesTaken >= 14:
-            return `You finished the game in ${goesTaken} goes! Better luck next time.`;
-        };
-    }
+  switch (true) {
+    case goesTaken < 10:
+      return `Congratulations! You finished the game in ${goesTaken} goes!`;
+    case goesTaken < 14:
+      return `Good effort! You finished the game in ${goesTaken} goes!`;
+    case goesTaken >= 14:
+      return `You finished the game in ${goesTaken} goes! Better luck next time.`;
+  }
+};
 
 // unmatched function: if tiles are not matched, flip them back and clear openTiles
 
 const tilesUnmatched = async () => {
-    tiles.forEach((tile) => {
-        tile.style.pointerEvents = "none"
-        });
-    await sleep(1200);
-    tiles.forEach((tile) => {
-        if(tile.classList.contains("open")) {
-            tile.classList.remove("open");
-         };
-        tile.style.pointerEvents = "";
-        });
-    openTiles = []
+  tiles.forEach((tile) => {
+    tile.style.pointerEvents = "none";
+  });
+  await sleep(1200);
+  tiles.forEach((tile) => {
+    if (tile.classList.contains("open")) {
+      tile.classList.remove("open");
     }
+    tile.style.pointerEvents = "";
+  });
+  openTiles = [];
+};
 
-  // matched function: if tiles are matched, keep them open and change to green and increment matchedPairs
+// matched function: if tiles are matched, keep them open and change to green and increment matchedPairs
 
 const tilesMatched = async () => {
-    tiles.forEach((tile) => {
-        tile.style.pointerEvents = "none"
-    });
-    await sleep(1200);
-    tiles.forEach((tile) => {
-        if(tile.classList.contains("open")) {
-            tile.classList.remove("open");
-            tile.classList.add("matched");
-        };
-        tile.style.pointerEvents = "";
-    })
-    openTiles = [];
-    matchedPairs += 1;
-
-    // if matchedPairs is 6 then end game with pop-up
-
-    if(matchedPairs === 6) {
-        message.innerHTML = renderFinalScreen(goesTaken);
-        await sleep(500);
-        notification.style.visibility = "visible";
+  tiles.forEach((tile) => {
+    tile.style.pointerEvents = "none";
+  });
+  await sleep(1200);
+  tiles.forEach((tile) => {
+    if (tile.classList.contains("open")) {
+      tile.classList.remove("open");
+      tile.classList.add("matched");
     }
+    tile.style.pointerEvents = "";
+  });
+  openTiles = [];
+  matchedPairs += 1;
+
+  // if matchedPairs is 6 then end game with pop-up
+
+  if (matchedPairs === 6) {
+    message.innerHTML = renderFinalScreen(goesTaken);
+    await sleep(500);
+    notification.style.visibility = "visible";
   }
+};
 
-
-
-// tileFlip function 
+// tileFlip function
 
 const tileFlip = (event) => {
+  const currentTile = event.currentTarget;
 
-    const currentTile = event.currentTarget;
+  //     flip up to two tiles and add to classlist
 
-    //     flip up to two tiles and add to classlist
+  if (openTiles.length < 2) {
+    openTiles.push(currentTile.className);
+    currentTile.classList.add("open");
+  }
 
-    if(openTiles.length < 2){
-        openTiles.push(currentTile.className);
-        currentTile.classList.add("open");
-    };
+  //     when two tiles are open, increment goesTaken and check for match
 
-    //     when two tiles are open, increment goesTaken and check for match
+  if (openTiles.length === 2) {
+    goesTaken += 1;
 
-    if(openTiles.length === 2){
-        goesTaken += 1;
-
-        if(openTiles[0] !== openTiles[1]){
-            tilesUnmatched();
-        }
-        else{
-            tilesMatched();
-        }
-        
-    };
-}
-
-
+    if (openTiles[0] !== openTiles[1]) {
+      tilesUnmatched();
+    } else {
+      tilesMatched();
+    }
+  }
+};
 
 // start game function: shuffle tiles and add event listener
 
 const startGame = () => {
-    const imageNumbers = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6"];
-    for(let i = tiles.length-1; i>=0; i--) {
-        const randomNumber = Math.floor(Math.random() * (i+1));
-        tiles[i].className += ` ${imageNumbers[randomNumber]}`;
-        tiles[i].childNodes[1].src = `images/${imageNumbers[randomNumber]}.svg`
-        imageNumbers.splice(randomNumber, 1);
-        tiles[i].addEventListener("click", tileFlip);
-    };
+  const imageNumbers = [
+    "1",
+    "1",
+    "2",
+    "2",
+    "3",
+    "3",
+    "4",
+    "4",
+    "5",
+    "5",
+    "6",
+    "6",
+  ];
+  for (let i = tiles.length - 1; i >= 0; i--) {
+    const randomNumber = Math.floor(Math.random() * (i + 1));
+    tiles[i].className += ` ${imageNumbers[randomNumber]}`;
+    tiles[i].childNodes[1].src = `images/${imageNumbers[randomNumber]}.svg`;
+    imageNumbers.splice(randomNumber, 1);
+    tiles[i].addEventListener("click", tileFlip);
+  }
 };
 
+// resetgame function to clear everything and start again
 
-// refreshgame function to clear everything and start again
+const resetGame = async () => {
+  tiles.forEach((tile) => {
+    tile.removeEventListener("click", tileFlip);
+    tile.className = "tile";
+  });
+  openTiles = [];
+  goesTaken = 0;
+  matchedPairs = 0;
 
-const refreshGame = async () => {
-    tiles.forEach((tile) => {
-        tile.removeEventListener("click", tileFlip);
-        tile.className = "tile";
-        });
-    openTiles = [];
-    goesTaken = 0;
-    matchedPairs = 0;
-
-    body.style.opacity = 0.5;
-    await sleep(200);
-    body.style.opacity = "";
-    await sleep(500);
-    startGame();
-}
-
+  body.style.opacity = 0.5;
+  await sleep(200);
+  body.style.opacity = "";
+  await sleep(500);
+  startGame();
+};
 
 // start game when page loads
 
 document.body.onload = startGame();
 
-
 // if New Game button is clicked, start new game
 
 newGame.addEventListener("click", () => {
-    refreshGame();
- });
-
+  resetGame();
+});
 
 //if play again button is clicked, start new game and get rid of pop up window
 
 playAgain.addEventListener("click", () => {
-    notification.style.visibility = "hidden";
-    refreshGame();
+  notification.style.visibility = "hidden";
+  resetGame();
 });
 
 // if close-popup x is clicked, close popup
 
 closePopup.addEventListener("click", () => {
-    notification.style.visibility = "hidden";
-})
+  notification.style.visibility = "hidden";
+});
